5705e62eb3191ee2e236260581157580fa15e324
bee1oss <begahaciyew@gmail.com>
2022-02-20 16:54:20 +0300

    Finish
    It is little atm banking program

--- src/Account.java
+++ src/Account.java
@@ -1,97 +1,42 @@
 public class Account {
-        private String hesapNo;
-        private double bakiye;
-        private String isim;
+        private String billNo;
+        private double balance;
+        private String name;
         
         private String email;
         
         private String telNo;
-        
-        public void paraYatir(double miktar){
-            bakiye += miktar;
+        //Burada Constructorun adi classin adiyla ayni olmak zorunda yoksa constructor olusturamayiz
+        /*public Account(){
             
-            System.out.println("Yeni bakiye:" + bakiye);
+            System.out.println("Kendi yazdigimiz Constructor...");
+        }*/
+        public Account(String billpNo,double balance,String name,String email, String telNo){
+             
+            this.billNo = billNo;
+            this.balance = balance;
+            this.name = name;
+            this.email = email;
+            this.telNo = telNo;
         }
-        public void paraCekme(double miktar){
-            if(miktar > 1500){
-                System.out.println("Bir gunde 1500'den fazla para cekemezsiniz");
+        
+        public void depMon(double quantity){
+            balance += quantity;
+            
+            System.out.println("New balance:" + balance);
+        }
+        public void witMon(double quantity){
+            if(quantity > 1500){
+                System.out.println("You cannot withdraw more than 1500 coins in one day.!");
             }
-            if(miktar>bakiye){
-                System.out.println("Yeterli bakiye yok..." + bakiye);
+            if(quantity>balance){
+                System.out.println("Not enough funds were found in your account..." + balance);
             }
             else{
-                bakiye -= miktar;
-                System.out.println("Cekme islemi basarili.. Yeni bakiye: " + bakiye );
+                balance -= quantity;
+                System.out.println("Withdraw successful...");
+                System.out.println(" New balance:" + balance);
             }
         }
-    /**
-     * @return the hesapNo
-     */
-    public String getHesapNo() {
-        return hesapNo;
-    }
-
-    /**
-     * @param hesapNo the hesapNo to set
-     */
-    public void setHesapNo(String hesapNo) {
-        this.hesapNo = hesapNo;
-    }
-
-    /**
-     * @return the bakiye
-     */
-    public double getBakiye() {
-        return bakiye;
-    }
-
-    /**
-     * @param bakiye the bakiye to set
-     */
-    public void setBakiye(double bakiye) {
-        this.bakiye = bakiye;
-    }
-
-    /**
-     * @return the isim
-     */
-    public String getIsim() {
-        return isim;
-    }
-
-    /**
-     * @param isim the isim to set
-     */
-    public void setIsim(String isim) {
-        this.isim = isim;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return the telNo
-     */
-    public String getTelNo() {
-        return telNo;
-    }
-
-    /**
-     * @param telNo the telNo to set
-     */
-    public void setTelNo(String telNo) {
-        this.telNo = telNo;
-    }
-    
+        
 }
--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+
 
 public class Test {
     public static void main(String[] args){
       
-       Account account1 = new Account();
+      // Account account1 = new Account();
+       Account account2 = new Account("123456" , 1000.0,"Bega H.","bega@gmail.com","+3752534534");
+     
+       int choose;
+       double dmon,wmon;
+       Scanner myObj = new Scanner(System.in);
+        
+       System.out.println("\n 1.Show balance in my account \n 2.Deposit process \n 3.Withdrawal process");
+        
+        System.out.print("Choose one of the options:");
        
-       account1.setHesapNo("123456");
+       
+       choose = myObj.nextInt();
+       
+       if(choose == 1){
+           
+       }
+       else if(choose == 2){
+           System.out.print("Enter the amount to deposit:");
+           dmon = myObj.nextDouble();
+           System.out.println(dmon+" Amount you deposit");
+            account2.depMon(dmon);
+       }
+       else if(choose == 3){
+           System.out.print("Enter the amount to withdrawal:");
+           wmon = myObj.nextDouble();
+           account2.witMon(wmon);
+       }
+       else{
+           System.out.println("You made a wrong choice");
+       }
+       /* account1.setHesapNo("123456");
        account1.setBakiye(1000.0);
        account1.setIsim("Burhan Nalbant");
        account1.setEmail("burhan@gmail.com");
-       account1.setTelNo("+375 25 341 51");
+       account1.setTelNo("+375 25 341 51");*/
+      
+      
     }
 }
